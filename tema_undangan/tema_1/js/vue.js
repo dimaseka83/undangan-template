793d2e1fd87ec30756a3bb971fa240b8100df81c
dhimas <dimaseka83@gmail.com>
2022-06-05 10:13:15 +0700

    ++

--- tema_undangan/tema_1/js/vue.js
+++ tema_undangan/tema_1/js/vue.js
@@ -1,60 +1,70 @@
 new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    data: {
-      arrow: false,
-      openInvitation: false,
-      ucapan: [],
-      form: [
-        {
-          nama: '',
-          ucapan: '',
-        }
-      ],
-      formBalasan: [
-        {
-          nama: '',
-          ucapan: '',
-        }
-      ],
-      dialog: false,
-    },
-    methods: {
-      scrollPlay() {
-        var audio = this.$refs.audioElm;
-        audio.play();
-        this.openInvitation = true;
-      },
-      kirimUcapan() {
-        this.ucapan.push({
-          nama: this.form.nama,
-          ucapan: this.form.ucapan,
-          reply: []
-        });
-        this.form = [{
-          nama: '',
-          ucapan: '',
-          reply: []
-        }];
-      },
-      kirimBalasan(id) {
-        this.ucapan[id].reply.push({
-          nama: this.formBalasan.nama,
-          ucapan: this.formBalasan.ucapan,
-        });
-        this.formBalasan = [{
-          nama: '',
-          ucapan: '',
-        }];
-      }
+  el: '#app',
+  vuetify: new Vuetify(),
+  data: {
+    arrow: false,
+    openInvitation: false,
+    ucapan: [],
+    form: [{
+      nama: '',
+      ucapan: '',
+    }],
+    formBalasan: [{
+      nama: '',
+      ucapan: '',
+    }],
+    dialog: false,
   },
-    
+  methods: {
+    scrollPlay() {
+      var audio = this.$refs.audioElm;
+      audio.play();
+      this.openInvitation = true;
+    },
+    kirimUcapan() {
+      this.ucapan.push({
+        nama: this.form.nama,
+        ucapan: this.form.ucapan,
+        reply: []
+      });
+      this.form = [{
+        nama: '',
+        ucapan: '',
+        reply: []
+      }];
+    },
+    kirimBalasan(id) {
+      this.ucapan[id].reply.push({
+        nama: this.formBalasan.nama,
+        ucapan: this.formBalasan.ucapan,
+      });
+      this.formBalasan = [{
+        nama: '',
+        ucapan: '',
+      }];
+    }
+  },
+
   computed: {
     totalUcapan() {
       return this.ucapan.length;
     },
-    displayMobile() {
-      return window.innerWidth < 768;
-    }
+    displayDesktop() {
+      return this.$vuetify.breakpoint.width >= 500
+    },
+    buttonGift() {
+      if (this.displayDesktop) {
+        return '';
+      } else {
+        return 'mb-5';
+      }
+    },
+    buttonLoad() {
+      if (this.displayDesktop) {
+        return '';
+      } else {
+        return 'mt-10';
+      }
+    },
   }
-  })
+})
