5f5bda9fc02a12ca7371bdd77e013446dbb21288
dhimas <dimaseka83@gmail.com>
2022-06-05 22:16:40 +0700

    ++

--- tema_undangan/tema_1/js/vue.js
+++ tema_undangan/tema_1/js/vue.js
@@ -4,6 +4,7 @@ new Vue({
   data: {
     arrow: false,
     openInvitation: false,
+    dataApi: [],
     ucapan: [],
     form: [{
       nama: '',
@@ -14,8 +15,21 @@ new Vue({
       ucapan: '',
     }],
     dialog: false,
+    daysCountdown: [
+      {
+        day: '',
+        hour: '',
+        minute: '',
+        second: '',
+      }
+    ],
   },
   methods: {
+    loadData() {
+      axios.get('https://merestui.com/api/vindirendra').then(response => {
+        this.dataApi = response.data.data;
+      });
+    },
     scrollPlay() {
       var audio = this.$refs.audioElm;
       audio.play();
@@ -45,6 +59,14 @@ new Vue({
     }
   },
 
+  created() {
+    this.loadData();
+  },
+
+  mounted() {
+    this.countdown();
+  },
+
   computed: {
     totalUcapan() {
       return this.ucapan.length;
@@ -66,5 +88,22 @@ new Vue({
         return 'mt-10';
       }
     },
-  }
-})
+        countdown() {
+      
+          setTimeout(() => {
+        var date = new Date();
+      var time = date.getTime();
+      var countDownDate = new Date(this.dataApi.wedding.countdown_readable).getTime();
+      var distance = countDownDate - time;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      this.daysCountdown.day = days;
+      this.daysCountdown.hour = hours;
+      this.daysCountdown.minute = minutes;
+          this.daysCountdown.second = seconds;        
+      }, 1000);
+    }
+  },
+});
