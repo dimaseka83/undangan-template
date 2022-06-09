32f000605207848d3b64734d63155b3c21e93b0b
dhimas <dimaseka83@gmail.com>
2022-06-09 21:57:09 +0700

    ++

--- tema_undangan/tema_4/js/vue.js
+++ tema_undangan/tema_4/js/vue.js
@@ -1,55 +1,134 @@
 new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    data() {
-        return {
-            dataApi: [],
-            loading: true,
-            value: 'home',
-            ucapan: [],
-            form: [{
-              nama: '',
-              ucapan: '',
-              kehadiran: '',
-            }],
-            kehadiran: ['Hadir', 'Tidak Hadir'],
-        }
-    },
-
-    methods: {
-        kirimUcapan() {
-            this.ucapan.push({
-              nama: this.form.nama,
-              ucapan: this.form.ucapan,
-              kehadiran: this.form.kehadiran,
-              // reply: []
-            });
-            // axios.post('https://merestui.com/api/'+this.dataApi.order.url+'/comment/store',{
-            //   ref_no: "1",
-            //   name: this.form.nama,
-            //   write_as: this.form.nama,
-            //   sosmed: this.form.nama,
-            //   comment: this.form.ucapan,
-            // })
-            this.form = [{
-              nama: '',
-              ucapan: '',
-              kehadiran: '',
-              // reply: []
-            }];
-          },
-        background() {
-            document.getElementsByClassName('bg-page1')[0].style.backgroundImage = "url('./img/black_image.png'), url('https://digition.id/wp-content/uploads/2022/02/55-1.jpg')";
-        }
-    },
-
-    mounted() {
-        this.background();
-    },
-
-    computed: {
-        totalUcapan() {
-            return this.ucapan.length;
-          },
+  el: '#app',
+  vuetify: new Vuetify(),
+  data() {
+    return {
+      dataApi: [],
+      loading: true,
+      value: 'home',
+      ucapan: [],
+      form: [{
+        nama: '',
+        ucapan: '',
+        kehadiran: '',
+      }],
+      kehadiran: ['Hadir', 'Tidak Hadir'],
+      displayDays: 0,
+      displayHours: 0,
+      displayMinutes: 0,
+      displaySeconds: 0,
     }
+  },
+
+  methods: {
+    kirimUcapan() {
+      this.ucapan.push({
+        nama: this.form.nama,
+        ucapan: this.form.ucapan,
+        kehadiran: this.form.kehadiran,
+        // reply: []
+      });
+      // axios.post('https://merestui.com/api/'+this.dataApi.order.url+'/comment/store',{
+      //   ref_no: "1",
+      //   name: this.form.nama,
+      //   write_as: this.form.nama,
+      //   sosmed: this.form.nama,
+      //   comment: this.form.ucapan,
+      // })
+      this.form = [{
+        nama: '',
+        ucapan: '',
+        kehadiran: '',
+        // reply: []
+      }];
+    },
+    loadData() {
+      axios.get('https://merestui.com/api/vindirendra').then(response => {
+        this.dataApi = response.data.data;
+        this.loading = false;
+      });
+    },
+    scrollPlay() {
+      var audio = this.$refs.audioElm;
+      audio.play();
+      this.openInvitation = true;
+    },
+    background() {
+        document.getElementsByClassName('bg-page1')[0].style.backgroundImage = "url('./img/black_image.png'), url('https://merestui.com/"+ this.dataApi.wedding.gallery_1 +"')";
+    },
+              openUrl(url){
+            window.open(url, '_blank');
+          },
+    showRemaining() {
+      const timer = setInterval(() => {
+        const now = new Date();
+        const tanggal = new Date(this.dataApi.wedding.countdown_readable).toString('yyyy-MM-dd');
+        const end = new Date(tanggal);
+        const distance = end.getTime() - now.getTime();
+
+        if (distance < 0) {
+          clearInterval(timer);
+          return;
+        }
+
+        const days = Math.floor(distance / this._days);
+        const hours = Math.floor((distance % this._days) / this._hours);
+        const minutes = Math.floor((distance % this._hours) / this._minutes);
+        const seconds = Math.floor((distance % this._minutes) / this._seconds);
+        this.displayMinutes = minutes < 10 ? '0' + minutes : minutes;
+        this.displaySeconds = seconds < 10 ? '0' + seconds : seconds;
+        this.displayHours = hours < 10 ? '0' + hours : hours;
+        this.displayDays = days < 10 ? '0' + days : days;
+      }, 1000);
+    }
+  },
+
+  created() {
+    this.loadData();
+  },
+
+  mounted() {
+    this.showRemaining();
+  },
+
+  updated() {
+    this.background();
+  },
+
+
+  computed: {
+    totalUcapan() {
+      return this.ucapan.length;
+    },
+    _seconds: () => 1000,
+    _minutes() {
+      return this._seconds * 60;
+    },
+    _hours() {
+      return this._minutes * 60;
+    },
+    _days() {
+      return this._hours * 24;
+    },
+        getDayOnlyAkad(){
+      if(this.loading == false){
+        return this.dataApi.wedding.tanggal_akad.split(' ')[0];
+      }
+    },
+        getTanggalAkad(){
+      if(this.loading == false){
+        return this.dataApi.wedding.tanggal_akad.split(",")[1];
+      }
+    },
+            getDayOnlyResepsi(){
+      if(this.loading == false){
+        return this.dataApi.wedding.tanggal_resepsi.split(' ')[0];
+      }
+    },
+    getTanggalResepsi(){
+      if(this.loading == false){
+        return this.dataApi.wedding.tanggal_resepsi.split(",")[1];
+      }
+    },
+  }
 });
