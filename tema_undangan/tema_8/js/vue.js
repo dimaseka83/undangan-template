e5748794d26524d8ebf66c85f3deb6206be1ed8e
dhimaseka <dimaseka83@gmail.com>
2022-06-15 16:25:26 +0700

    ++

---
+++ tema_undangan/tema_8/js/vue.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+
+    computed: {
+        displayDesktop() {
+            return this.$vuetify.breakpoint.width >= 500
+          },
+    }
+});
