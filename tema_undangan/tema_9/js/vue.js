cf8fde2e4abb0385a55dfefc0e1c88e373dea5fb
dhimaseka <dimaseka83@gmail.com>
2022-06-22 11:13:08 +0700

    ++

--- tema_undangan/tema_9/js/vue.js
+++ tema_undangan/tema_9/js/vue.js
@@ -6,7 +6,7 @@ new Vue({
             dataApi: [],
             dialog: false,
             dialog1: false,
-            openInvitation: false,
+            openInvitation: true,
             loading: true,
             displayDays: 0,
             displayHours: 0,
